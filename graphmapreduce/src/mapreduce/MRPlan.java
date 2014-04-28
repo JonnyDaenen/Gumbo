9aba023ad2c4c0888fa93d08220efa2532944692
Jonny Daenen <jonnydaenen@gmail.com>
2014-04-28 08:14:33 +0000

    fixed temp dir removal

--- graphmapreduce/src/mapreduce/MRPlan.java
+++ graphmapreduce/src/mapreduce/MRPlan.java
@@ -1,26 +1,53 @@
 package mapreduce;
 
+import guardedfragment.mapreduce.GFMRlevel1Example2;
+
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 
+/**
+ * Provides a way to execute a set of MR jobs that are possibly dependent.
+ * All jobs are executed in a seperate thread, which is polled for completion.
+ * Input, output, scratch and temporary directories are to be specified.
+ * Scratch and temp folders are removed after completion, unless suppressed with the supplied preference.
+ * 
+ * The output and scratch folders should belong to one plan.
+ * The temp folders should be located inside the scratch folder, but can later be used
+ * to provide a more early temp folder deletion mechanism. 
+ * 
+ * A summary of the plan is placed inside the output folder.
+ * 
+ * @author Jonny Daenen
+ *
+ */
 public class MRPlan extends Configured {
+	
 
-	protected String name = "FonjoAlphaPlan"; // FUTURE change
+	private static final Log LOG = LogFactory.getLog(MRPlan.class);
+
+	protected String name = "FonjoPlan"; // FUTURE change
 	protected String inputFolder; // FUTURE I think this should be a set
-	protected String outputFolder; // FUTURE I think this should be a set
+	protected String outputFolder;
+	protected String scratchFolder;
 	protected Set<ControlledJob> jobs;
 	protected Set<Path> tempdirs;
+	
+	boolean deleteTmpDirs;
 
 	public MRPlan() {
 		jobs = new HashSet<ControlledJob>();
 		tempdirs = new HashSet<Path>();
+		deleteTmpDirs = true;
 	}
 
 	/**
@@ -50,18 +77,21 @@ public class MRPlan extends Configured {
 		try {
 
 			// 1. create JobControl
-			JobControl jc = new JobControl(name); // TODO name
+			JobControl jc = new JobControl(name);
 
 			// 2. we add all the controlled jobs to the job control
 			// note that this can also be done in 1 go by using a collection
 			jc.addJobCollection(jobs);
 
 			// 3. we execute the jobcontrol in a Thread
-			Thread workflowThread = new Thread(jc, "Fronjo-Workflow-Thread"); // TODO change
-			workflowThread.setDaemon(true); // TODO what's this?
+			Thread workflowThread = new Thread(jc, "Fronjo-Workflow-Thread_"+name);
+			workflowThread.setDaemon(true); // will not avoid JVM to shutdown
+			
+			LOG.info("Starting Job-control thread: " + workflowThread.getName());
 			workflowThread.start();
 
 			// 4. we wait for it to complete
+			LOG.info("Waiting for thread to complete: " + workflowThread.getName());
 			while (!jc.allFinished()) {
 				Thread.sleep(500);
 			}
@@ -69,37 +99,54 @@ public class MRPlan extends Configured {
 			// 5. clean up in case of failure
 
 			if (jc.getFailedJobList().size() > 0) {
-				System.err.println(jc.getFailedJobList().size() + " jobs failed!");
+				LOG.error(jc.getFailedJobList().size() + " jobs failed!");
 
 				for (ControlledJob job : jc.getFailedJobList()) {
-					System.err.println("\t" + job.getJobName() + " failed.");
+					LOG.error("\t" + job.getJobName() + " failed.");
 				}
 			} else {
 				success = true;
-				System.err.println("SUCCESS: all jobs (" + jc.getSuccessfulJobList().size() + ") completed!");
+				LOG.info("SUCCESS: all jobs (" + jc.getSuccessfulJobList().size() + ") completed!");
 			}
 
 		} finally {
 			// remove the temp dirs
-			//TODO deleteTmpDirs();
+			if (deleteTmpDirs)
+				deleteTmpDirs();
 		}
 
 		return success;
 	}
 
 	/**
-	 * Delete all temporary dirs from the file system
-	 * FIXME this is broken
+	 * Delete all temporary dirs from the file system.
 	 * 
 	 * @throws IOException
 	 */
-	@SuppressWarnings("unused")
 	private void deleteTmpDirs() {
 		FileSystem fs;
+		
+		// delete temp dirs
 		try {
-			fs = FileSystem.get(getConf());
+			fs = FileSystem.get(new Configuration());
 
 			for (Path p : tempdirs) {
+				System.out.println("Checking: "+ p);
+				if (fs.exists(p)) {
+					fs.delete(p, true); // delete recursive
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("WARNING: problem deleting temporary folders!");
+			e.printStackTrace();
+		}
+		
+		// delete scratch dir
+		try {
+			fs = FileSystem.get(new Configuration());
+
+			for (Path p : tempdirs) {
+				System.out.println("Checking: "+ p);
 				if (fs.exists(p)) {
 					fs.delete(p, true); // delete recursive
 				}
@@ -117,6 +164,9 @@ public class MRPlan extends Configured {
 	@Override
 	public String toString() {
 		String output = "";
+		
+		
+		// TODO add date
 		
 		// name
 		output += System.getProperty("line.separator");
@@ -158,6 +208,8 @@ public class MRPlan extends Configured {
 		output += System.getProperty("line.separator");
 		output += "output: " + outputFolder;
 		output += System.getProperty("line.separator");
+		output += "scratch: " + scratchFolder;
+		output += System.getProperty("line.separator");
 		
 		output += "Temp-dirs: ";
 		output += System.getProperty("line.separator");
@@ -184,6 +236,24 @@ public class MRPlan extends Configured {
 
 	public void setOutputFolder(String outputFolder) {
 		this.outputFolder = outputFolder;
+	}
+	
+	public String getScratchFolder() {
+		return scratchFolder;
+	}
+	
+	public void setScratchFolder(String scratchFolder) {
+		this.scratchFolder = scratchFolder;
+	}
+
+	/**
+	 * Toggles deletion of tmp dirs after execution. 
+	 * When enabled, they are also deleted when something has gone wrong.
+	 * 
+	 * @param deleteTmpDirs
+	 */
+	public void setDeleteTmpDirs(boolean deleteTmpDirs) {
+		this.deleteTmpDirs = deleteTmpDirs;
 	}
 
 }
