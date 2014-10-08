87c92f7f2fc2de638a585471a7b50dedd9c0b07b
Jonny Daenen <jonnydaenen@gmail.com>
2014-10-08 08:27:50 +0000

    incomplete: test-drive writer objects

--- graphmapreduce/examples/mapreduce/hadoop/readwrite/ProjectionMapper.java
+++ graphmapreduce/examples/mapreduce/hadoop/readwrite/ProjectionMapper.java
@@ -4,6 +4,8 @@ package mapreduce.hadoop.readwrite;
 // vv MaxTemperatureMapper
 import java.io.IOException;
 
+import mapreduce.guardedfragment.planner.structures.data.Tuple;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -22,26 +24,7 @@ public class ProjectionMapper extends
 		
 		Tuple t = new Tuple(value.toString());
 		
-		if(vals.length == 0 )
-			return;
-		
-		String line = vals[vals.length-1];
-		String year = line.substring(15, 19);
-		int airTemperature;
-		
 
-		
-		if (line.charAt(87) == '+') { // parseInt doesn't like leading plus
-										// signs
-			airTemperature = Integer.parseInt(line.substring(88, 92));
-		} else {
-			airTemperature = Integer.parseInt(line.substring(87, 92));
-		}
-		String quality = line.substring(92, 93);
-		if (airTemperature != MISSING && quality.matches("[01459]")) {
-			// System.out.println(year);
-			context.write(new Text(year), new IntWritable(airTemperature));
-		}
 	}
 }
 // ^^ MaxTemperatureMapper
