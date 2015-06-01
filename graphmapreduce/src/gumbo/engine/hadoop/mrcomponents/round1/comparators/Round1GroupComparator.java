c73ba89dec553c01abfd9930d667241a140465bb
Jonny Daenen <jonnydaenen@gmail.com>
2015-06-01 09:38:31 +0000

    optimized groupcomparator

--- graphmapreduce/src/gumbo/engine/hadoop/mrcomponents/round1/comparators/Round1GroupComparator.java
+++ graphmapreduce/src/gumbo/engine/hadoop/mrcomponents/round1/comparators/Round1GroupComparator.java
@@ -90,24 +90,24 @@ public class Round1GroupComparator extends WritableComparator {
 		return result;
 	}
 
-//	@Override
-//	public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-//		try {
-//			ByteBuffer bb1 = ByteBuffer.wrap(b1,s1+1,l1-1); // first byte is length, so we skip it
-//			CharBuffer charbuf1 = decoder.decode(bb1);
-//
-//			ByteBuffer bb2 = ByteBuffer.wrap(b2,s2+1,l2-1);
-//			CharBuffer charbuf2 = decoder.decode(bb2);
-//
-//			int val =  compareBuffers(charbuf1, charbuf2);
-//			return val;
-//		} catch (Exception e) {
-//
-//		}
-//
-//		// fallback 
-//		return super.compare(b1, s1, l1, b2, s2, l2);
-//	}
+	@Override
+	public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+		try {
+			ByteBuffer bb1 = ByteBuffer.wrap(b1,s1+1,l1-1); // first byte is length, so we skip it
+			CharBuffer charbuf1 = decoder.decode(bb1);
+
+			ByteBuffer bb2 = ByteBuffer.wrap(b2,s2+1,l2-1);
+			CharBuffer charbuf2 = decoder.decode(bb2);
+
+			int val =  compareBuffers(charbuf1, charbuf2);
+			return val;
+		} catch (Exception e) {
+
+		}
+
+		// fallback 
+		return super.compare(b1, s1, l1, b2, s2, l2);
+	}
 
 	//	@Override
 	//	public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
