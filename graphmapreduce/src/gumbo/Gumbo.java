9acdf64163d75e6d76e8189dc9f423320f6b1560
Jonny Daenen <jonnydaenen@gmail.com>
2015-07-02 12:53:32 +0000

    re-enabled main gumbo

--- graphmapreduce/src/gumbo/Gumbo.java
+++ graphmapreduce/src/gumbo/Gumbo.java
@@ -139,13 +139,13 @@ public class Gumbo extends Configured implements Tool {
 		System.out.println(plan);
 
 		// --- fresh
-		FileMappingExtractor fme = new FileMappingExtractor(false);
-		RelationFileMapping mapping2 = fme.extractFileMapping(plan.getFileManager());
-		Grouper grouper = new Grouper(new KeyGrouper(new GGTCostCalculator(new HadoopCostSheet(mapping2, settings))));
-
-		grouper.group(plan.getPartitions());
-
-		System.exit(0);
+//		FileMappingExtractor fme = new FileMappingExtractor(false);
+//		RelationFileMapping mapping2 = fme.extractFileMapping(plan.getFileManager());
+//		Grouper grouper = new Grouper(new KeyGrouper(new GGTCostCalculator(new HadoopCostSheet(mapping2, settings))));
+//
+//		grouper.group(plan.getPartitions());
+//
+//		System.exit(0);
 		// ---
 
 
