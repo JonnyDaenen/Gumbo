dc0b633f849ede1e7e4144af3f1b1333a2b187b1
Jonny Daenen <jonnydaenen@gmail.com>
2016-02-04 15:37:53 +0000

    improved cost model

--- graphmapreduce/src/gumbo/engine/general/grouper/costmodel/MRSettings.java
+++ graphmapreduce/src/gumbo/engine/general/grouper/costmodel/MRSettings.java
@@ -9,14 +9,15 @@ import gumbo.engine.general.settings.AbstractExecutorSettings;
  */
 public class MRSettings {
 
+	// mark
 	// costs (s per MB)
-	protected double cost_local_r = 0.016;
-	protected double cost_local_w = 0.1; // 0.06
-	protected double cost_hdfs_w = 0.21; // done
-	protected double cost_hdfs_r = 0.09; // 0.06
+	protected double cost_local_r = 0.03;
+	protected double cost_local_w = 0.085; // 0.06
+	protected double cost_hdfs_w = 0.25; // done
+	protected double cost_hdfs_r = 0.15; // 0.06
 	protected double cost_transfer = 0.017; // done
 	protected double cost_sort = 52;
-	protected double cost_red = 0.21; // done
+	protected double cost_red = 0.25; // done FIXME make this same as hdfs write
 	protected double cost_transfer_penalty = 0.005;
 	
 
